72b6e055d6287fc507e6a4eeeffa115445ea543f
luffycodes <luffycodes@users.noreply.github.com>
2016-10-23 18:33:07 +0530

    Create CYLINDER.cpp

---
+++ CYLINDER.cpp
@@ -0,0 +1,27 @@
+# include <cstdio>
+ 
+#define PI 3.1415926535897931159979635
+#define MIN(a,b) ((a)<(b)?(a):(b))
+ 
+using namespace std;
+ 
+int main()
+{
+	int a, b;
+	double h1, h2, r1, r2, v1, v2;
+	while(scanf("%d%d",&a,&b) && a)
+	{
+		r1 = a/(2.0*PI);
+		h1 = b - 2*r1;
+		r2 = MIN(b/(2.0*(PI+1.0)),a/2.0);
+		h2 = a;
+		v1 = (PI*r1*r1*h1);
+		v2 = (PI*r2*r2*h2);
+		if(v1 > v2) 
+			printf("%.3lf\n",v1);
+		else 
+			printf("%.3lf\n",v2);
+ 
+	}
+	return 0;
+}
